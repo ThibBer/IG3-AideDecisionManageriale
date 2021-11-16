5c77b410156f2e56d44fb3359c63c848e643e52a
Victor Santelé <Vic16@hotmail.be>
2021-11-16 16:41:09 +0100

    ajout pi + tableau valeurs test carré unité

--- partie1.js
+++ partie1.js
@@ -25,10 +25,42 @@ function Uns(suite, m) {
 function distanceCarre(xn, xn1, xn2, xn3) {
     return Math.pow(xn3 - xn1, 2) + Math.pow(xn2 - xn, 2);
 }
+function F(x) {
+    if (x <= 1) {
+        return Math.PI * x - (8 / 3) * Math.pow(x, 3 / 2) + Math.pow(x, 2) / 2;
+    }
+    return (
+        1 / 3 +
+        (Math.PI - 2) * x +
+        4 * Math.sqrt(x - 1) +
+        (8 / 3) * Math.pow(x - 1, 3 / 2) -
+        Math.pow(x, 2) / 2 -
+        4 * x * Math.acos(1 / Math.sqrt(x))
+    );
+}
+function* genPi() {
+    let n = 1;
+    let oldFx = 0;
+    let fx;
+    while (n <= 20) {
+        fx = F(n / 10);
+        let pi = fx - oldFx;
+        yield pi;
+        n += 1;
+        oldFx = fx;
+    }
+}
 
 function testCarréUnite(suiteUns) {
     const nbGroupes = parseInt(suiteUns.length / 4, 10);
-
+    const valeurs = {};
+    const pi = genPi();
+    for (let x = 1; x <= 20; x += 1) {
+        valeurs[x] = {
+            ri: 0,
+            pi: pi.next().value,
+        };
+    }
     for (let n = 0; n < nbGroupes; n += 3) {
         const distance = distanceCarre(
             suiteUns[n],
@@ -36,16 +68,19 @@ function testCarréUnite(suiteUns) {
             suiteUns[n + 2],
             suiteUns[n + 3]
         );
+        const val = Math.ceil(distance * 10);
+        valeurs[val].ri += 1;
     }
 }
 
-const m = 8;
-const a = 5;
-const c = 5;
-const x0 = 3;
+const m = 63;
+const a = 22;
+const c = 4;
+const x0 = 19;
 
 const suite = generationSuiteAleatoire(m, a, c, x0);
 const suiteUn = Uns(suite, m);
 
 console.log(suite);
 console.log(suiteUn);
+testCarréUnite(suiteUn);

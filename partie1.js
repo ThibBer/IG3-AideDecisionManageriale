b5ba6802bc9cd223ef20b5737e8a6307af71a9ec
Thibaut <thibaut2001>
2021-11-16 10:19:16 +0100

    Génération suite aléatoire + début test carré-unité

--- partie1.js
+++ partie1.js
@@ -16,6 +16,31 @@ function formuleCongruentielleLinéaireMixte(m, a, c, xn){
     return (a * xn + c) % m
 }
 
-const suite = generationSuiteAleatoire(8, 5, 5, 3);
-console.log(suite)
+/*Génére les Un à partir des Xn*/
+function Uns(suite, m){
+    return suite.map(xn => xn / m);
+}
 
+/*Calcul la distance entre les points*/
+function distanceCarre(xn, xn1, xn2, xn3){
+    return Math.pow(xn3 - xn1, 2) + Math.pow(xn2 - xn, 2);
+}
+
+function testCarréUnite(suiteUns){
+    const nbGroupes = parseInt(suiteUns.length / 4);
+
+    for(let n = 0; n < nbGroupes; n += 3){
+        const distance = distanceCarre(suiteUns[n], suiteUns[n + 1], suiteUns[n + 2], suiteUns[n + 3]);
+    }
+}
+
+const m = 8;
+const a = 5;
+const c = 5;
+const x0 = 3;
+
+const suite = generationSuiteAleatoire(m, a, c, x0);
+const suiteUn = Uns(suite, m);
+
+console.log(suite);
+console.log(suiteUn);
